bacc710c09df959e0ec54fdf146bebb1b54f1d4d
Kiril <kirill.maneychik@gmail.com>
2020-03-03 09:32:32 +0100

    Created emailParser

--- cw1/Program.cs
+++ cw1/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace cw1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://www.pja.edu.pl/");
+            if(response.IsSuccessStatusCode)
+            {
+                var str = await response.Content.ReadAsStringAsync();
+
+                var emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", RegexOptions.IgnoreCase);
+                var emailMatches = emailRegex.Matches(str);
+
+                foreach (var emailMatch in emailMatches)
+                {
+                    Console.WriteLine(emailMatch);
+                }
+            }
         }
     }
 }
